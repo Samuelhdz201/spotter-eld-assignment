3880ee758905eef69e1066db5c39aa71cd7668ca
Samuel <romario.samuel201@gmail.com>
2026-04-14 02:32:46 -0600

    fix: corrected admin urls attribute

--- backend/trip_planner/urls.py
+++ backend/trip_planner/urls.py
@@ -10,6 +10,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.view),
+    path('admin/', admin.site.urls),
     path('api/', include('trip_planner.urls')), 
 ]
